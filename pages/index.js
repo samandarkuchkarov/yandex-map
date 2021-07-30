a46ac1cae6df0ae4ecfe1054c8842d4b4d68a862
samandar <samandar8939522@gmail.com>
2021-07-30 11:57:57 +0300

    f

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,66 @@
 
 import styles from '../styles/Home.module.css'
+import React from 'react';
 import { YMaps,Map, GeolocationControl, SearchControl,ObjectManager,RouteButton,RulerControl,TypeSelector, ZoomControl,FullscreenControl } from 'react-yandex-maps';
 import Data from '../location.json'
+import copy from 'copy-to-clipboard';
+
+
 export default function Home() {
+
+  const searchRef = React.useRef(null);
+  const [text,setText] = React.useState()
+  const [coordinates,setCoor] = React.useState(['',''])
+
+  React.useEffect(() => {
+    if (text && searchRef.current) {
+        searchRef.current.search(text);
+    }
+  }, [text]);
+
+  const doubleClick = (inst) =>{
+    if(inst){
+      inst.events.add('dblclick', (e)=>{  
+        setText(e.originalEvent.map._bounds[1][0]+','+e.originalEvent.map._bounds[1][1])
+      })
+    }
+  }
+  function copyToClipboard(e) {
+    copy(e, {
+      debug: true,
+      message: 'Press #{key} to copy',
+    });
+    
+  };
+
+
   return (
-    <div style={{display:'flex',alignItems:'center',justifyContent:'center',height:'100vh',width:'100vw'}}> 
-    <YMaps   query={{
-      apikey: '8140bb7c-9efa-44ca-8ec9-70eb503948d1',
-    }} >
-    <Map width='500px' height='500px' defaultState={{ center: [41.298916, 69.360232], zoom: 12 }} >
-      <GeolocationControl options={{float:'right',maxWidth:'40px'}}/>
-      <SearchControl />
+    <div className='container'> 
+      <div onClick={()=>{copyToClipboard(`${coordinates[0]}  ${coordinates[1]}`)}} className="coordinate">{`${coordinates[0]},  ${coordinates[1]}` }</div>
+
+    <YMaps    query={{apikey: '8140bb7c-9efa-44ca-8ec9-70eb503948d1'}} >
+      <Map  instanceRef={inst => {doubleClick(inst)}}
+        width='100vw' height='100vh' defaultState={{ center: [41.298916, 69.360232], zoom: 12 }} >
+        <GeolocationControl options={{float:'right',maxWidth:'40px'}}/>
+        <SearchControl instanceRef={ref => {
+                if (ref) searchRef.current = ref; 
+                if(ref){
+                  ref.events.add('resultselect', async() =>{
+                    var result = ref.getResult(0)
+                    result.then(function (res) {
+                      setCoor(res.geometry._coordinates)
+                  }, function (err) {
+                      console.log("Error");
+                  });
+                }, this);
+                }
+              
+              }} />
       <RouteButton/>
       <RulerControl/>
       <TypeSelector/>
       <ZoomControl/>
-      <FullscreenControl/>
+      {/* <FullscreenControl/> */}
       <ObjectManager
             objects={{
               openBalloonOnClick: true
@@ -57,3 +102,4 @@ export default function Home() {
     </div>
   )
 }
+
